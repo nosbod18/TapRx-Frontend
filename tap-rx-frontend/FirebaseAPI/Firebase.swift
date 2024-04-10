470a746e7c6ce17a2d5570c786bbeb6b6c612670
Drew Clutes <aack2f@umsystem.edu>
2024-04-10 00:08:22 -0500

    API testing for dependants, users and medications

--- tap-rx-frontend/FirebaseAPI/Firebase.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  tap-rx-frontend
-//
-//  Created by Drew Clutes on 2/19/24.
-//
-
-import Foundation
--- tap-rx-frontend/FirebaseAPI/FirebaseAuth.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FirebaseAuth.swift
-//  tap-rx-frontend
-//
-//  Created by Drew Clutes on 2/19/24.
-//
-
-
